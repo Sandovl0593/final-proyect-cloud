252b2c318503b8f2d80a7eec352ba9de77f26566
Sandovl0593 <adrian.sandoval@utec.edu.pe>
2023-07-05 00:19:26 -0500

    init proyect

--- extract_subtitle.py
+++ extract_subtitle.py
@@ -33,7 +33,7 @@ def lambda_handler(event, context):
         Media={
             'MediaFileUri': f's3://{video_dir}'
         },
-        OutputBucketName=f'subtitle-{name_video}',
+        OutputBucketName=f'subtitiled_videos',
         OutputKey=output_key
     )
     return {
